6e02e5160b26aca8e98f9d9568dd5b61ab7b9635
Paul <pje224@nyu.edu>
2020-10-02 01:34:39 -0400

    Introduce image assets and implement rudimentary navbar animation

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+if (true) {
+  console.log("hellow rold");
+}
+
+function navEnter() {
+  document.getElementById("navBar").style.left = '0';
+}
+
+function navExit() {
+  document.getElementById("navBar").style.left = '-30vw';
+}
