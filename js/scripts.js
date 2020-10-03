0df7e1b0faf4d662d703c79800f03ca354714fd3
Paul <pje224@nyu.edu>
2020-10-02 23:18:57 -0400

    Use Intersection Observer API to toggle event listeners for title CSS

--- js/scripts.js
+++ js/scripts.js
@@ -4,40 +4,43 @@ let navModal = document.getElementsByClassName("navModal")[0];
 let navBar = document.getElementById("navBar");
 
 function navOpen() {
-  navModal.style.display = 'block';
-  navBar.style.left = '0';
+  navModal.style.display = "block";
+  navBar.style.left = "0";
 }
 
 function navClose() {
-  navModal.style.display = 'none';
-  navBar.style.left = '-70vw';
+  navModal.style.display = "none";
+  navBar.style.left = "-70vw";
 }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == navModal) {
     navClose();
   }
-}
-
-//ObserverAPI call to see if title is visible
-
-/* 
-
-let titleAnimator = function(entries) {
-  console.log(entries[0].isIntersecting)
 };
 
-let observer = new IntersectionObserver(titleAnimator);
-observer.observe(title); */
+//function to add transform-scale CSS to title once the user has scrolled down
 
-let title = document.getElementsByClassName('title')[0];
+let title = document.getElementsByClassName("title")[0];
 
-window.addEventListener('scroll', function(e){
+let upscaler = function (event) {
   if (window.pageYOffset > 150) {
-    title.className += ' upscale';
+    title.className += " upscale";
   } else {
-    title.className = 'title'
+    title.className = "title";
   }
-})
+};
 
-//need to add a remove event listener when title not visible
+//ObserverAPI call removes event listener when title is not visible to save resources
+
+let titleVisibility = function (entries) {
+  let visible = entries[0].isIntersecting;
+  if (visible) {
+    window.addEventListener("scroll", upscaler, true);
+  } else {
+    window.removeEventListener("scroll", upscaler, true);
+  }
+};
+
+let observer = new IntersectionObserver(titleVisibility);
+observer.observe(title);
