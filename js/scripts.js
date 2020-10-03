26c5f873ed13cf39319a100d7e1d8616a2689aa4
Paul <pje224@nyu.edu>
2020-10-03 15:56:40 -0400

    JS for triggering greyscale CSS animation on section header

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,7 @@ window.onclick = function (event) {
   }
 };
 
-//function to add transform-scale CSS to title once the user has scrolled down
+//function to add transform-scale CSS to title once the user has scrolled down X amount
 
 let title = document.getElementsByClassName("title")[0];
 
@@ -42,5 +42,23 @@ let titleVisibility = function (entries) {
   }
 };
 
-let observer = new IntersectionObserver(titleVisibility);
-observer.observe(title);
+let titleObserver = new IntersectionObserver(titleVisibility);
+titleObserver.observe(title);
+
+//function to colorize section header
+
+let bwHeader = document.getElementsByClassName("marlon")[0];
+
+let headerVisibility = function (entries) {
+  let amountVisible = entries[0].intersectionRatio;
+  if (amountVisible >= 0.5) {
+    bwHeader.className += " colorized";
+  } else {
+    bwHeader.className = "marlon";
+  }
+};
+
+let headerObserver = new IntersectionObserver(headerVisibility, {
+  threshold: 0.5,
+});
+headerObserver.observe(bwHeader);
