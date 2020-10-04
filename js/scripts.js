151ed56f2213e30cb11fcd3cf87495d438de72e0
Paul <pje224@nyu.edu>
2020-10-04 16:35:57 -0400

    Remove modal and create switchable zoom icon, need to DRY and create toggle

--- js/scripts.js
+++ js/scripts.js
@@ -16,14 +16,13 @@ function navClose() {
 let lightFG = document.getElementsByClassName('lightFG')[0];
 let lightBG = document.getElementsByClassName('lightBG')[0];
 let lightButton = document.getElementsByClassName('lightButton')[0];
-let lightModal = document.getElementsByClassName('lightModal')[0];
+let zoomOutSVG = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zM7 9h5v1H7z"/></svg>';
+let zoomInSVG = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zm2.5-4h-2v2H9v-2H7V9h2V7h1v2h2v1z"/></svg>';
 
 function focusBlur() {
   lightFG.className += " focused";
   lightBG.className += " blurred";
-  lightButton.className += " blurred";
-  lightModal.className += " darkenFrame";
-  document.body.style['overflow-y'] = "hidden";
+  lightButton.innerHTML = zoomOutSVG;
 }
 
 //onclick was not working with safari, ontouchstart did
@@ -35,9 +34,7 @@ if ("ontouchstart" in window) {
     } else if ((event.target == lightFG)||(event.target == lightModal)) {
       lightFG.className = "lightFG";
       lightBG.className = "lightBG";
-      lightButton.className = "lightButton";
-      lightModal.className = "lightModal";
-      document.body.style['overflow-y'] = "visible";
+      lightButton.innerHTML = zoomInSVG;
     }
   };
 } else {
@@ -47,9 +44,7 @@ if ("ontouchstart" in window) {
     } else if ((event.target == lightFG)||(event.target == lightModal)) {
       lightFG.className = "lightFG";
       lightBG.className = "lightBG";
-      lightButton.className = "lightButton";
-      lightModal.className = "lightModal";
-      document.body.style['overflow-y'] = "visible";
+      lightButton.innerHTML = zoomInSVG;;
     }
   };
 }
