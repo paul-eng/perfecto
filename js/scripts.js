89c85c6d5f2f1a4346872234345213bbdf855c78
Paul <pje224@nyu.edu>
2020-10-04 19:47:42 -0400

    ontouchend worked, see if its snappier as an ontouchstart event

--- js/scripts.js
+++ js/scripts.js
@@ -13,11 +13,13 @@ function navClose() {
   navBar.style.left = "-70vw";
 }
 
-let lightFG = document.getElementsByClassName('lightFG')[0];
-let lightBG = document.getElementsByClassName('lightBG')[0];
-let lightButton = document.getElementsByClassName('lightButton')[0];
-let zoomOutSVG = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zM7 9h5v1H7z"/></svg>';
-let zoomInSVG = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zm2.5-4h-2v2H9v-2H7V9h2V7h1v2h2v1z"/></svg>';
+let lightFG = document.getElementsByClassName("lightFG")[0];
+let lightBG = document.getElementsByClassName("lightBG")[0];
+let lightButton = document.getElementsByClassName("lightButton")[0];
+let zoomOutSVG =
+  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zM7 9h5v1H7z"/></svg>';
+let zoomInSVG =
+  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zm2.5-4h-2v2H9v-2H7V9h2V7h1v2h2v1z"/></svg>';
 
 function addFocus() {
   lightFG.className += " focused";
@@ -32,33 +34,34 @@ function removeFocus() {
 }
 
 function focusBlur() {
-  document.getElementsByClassName('focused').length > 0 ? removeFocus() : addFocus();
+  document.getElementsByClassName("focused").length > 0
+    ? removeFocus()
+    : addFocus();
 }
 
 //onclick was not working with safari, ontouchstart did
-
 
 if ("ontouchstart" in window) {
   window.ontouchstart = function (event) {
     if (event.target == navModal) {
       navClose();
-    } 
-  };
+    }
 
-  window.ontouchend = function (event) {
-    navChapter = event.target.className == 'navChapter'
+    navChapter = event.target.className == "navChapter";
     if (navChapter) {
       let chapter = event.target.href;
       navClose();
       window.location.href = chapter;
     }
-  }
+  };
+  //ontouchstart on mobile was cancelling out the regular actoin of the anchor tags used for navChapters and the navBar closed but page wouldn't jump to correct section. Manually find href and go with JS
+  window.ontouchend = function (event) {};
 } else {
   window.onclick = function (event) {
-    navChapter = event.target.className == 'navChapter'
-    if ((event.target == navModal)||navChapter) {
+    navChapter = event.target.className == "navChapter";
+    if (event.target == navModal || navChapter) {
       navClose();
-    } 
+    }
   };
 }
 
