818490edbc2c89b12de680f3cf18cc1079e50034
Paul <pje224@nyu.edu>
2020-10-04 21:19:25 -0400

    Rollback to basic anchor tags

--- js/scripts.js
+++ js/scripts.js
@@ -170,37 +170,3 @@ let vanisher = function (entries) {
 let quoteVanisher = new IntersectionObserver(vanisher, { threshold: 0 });
 
 allQuotes.forEach((quote) => quoteVanisher.observe(quote));
-
-//functions to manually implement scroll-behavior:smooth across all browsers
-
-(function () {
-  scrollTo();
-})();
-
-function scrollTo() {
-  const links = document.querySelectorAll(".scroll");
-  links.forEach((each) => (each.onclick = scrollAnchors));
-}
-
-function scrollAnchors(e, respond = null) {
-  navClose();
-  const distanceToTop = (el) => Math.floor(el.getBoundingClientRect().top);
-  e.preventDefault();
-  var targetID = respond
-    ? respond.getAttribute("href")
-    : this.getAttribute("href");
-  const targetAnchor = document.querySelector(targetID);
-  if (!targetAnchor) return;
-  const originalTop = distanceToTop(targetAnchor);
-  window.scrollBy({ top: originalTop, left: 0, behavior: "smooth" });
-  const checkIfDone = setInterval(function () {
-    const atBottom =
-      window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
-    if (distanceToTop(targetAnchor) === 0 || atBottom) {
-      targetAnchor.tabIndex = "-1";
-      targetAnchor.focus();
-      window.history.pushState("", "", targetID);
-      clearInterval(checkIfDone);
-    }
-  }, 100);
-}
