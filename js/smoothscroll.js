c4af52f370634e825e5022079433e5fa2ae7e960
Paul <pje224@nyu.edu>
2021-03-30 02:53:58 -0400

    Try diff approach in smooth js file

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,23 +1,3 @@
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-  typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (global = global || self, factory(global.seamless = {}));
-}(this, function (exports) { 'use strict';
-
-  /*! *****************************************************************************
-  Copyright (c) Microsoft Corporation. All rights reserved.
-  Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-  this file except in compliance with the License. You may obtain a copy of the
-  License at http://www.apache.org/licenses/LICENSE-2.0
-
-  THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-  KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
-  WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
-  MERCHANTABLITY OR NON-INFRINGEMENT.
-
-  See the Apache Version 2.0 License for specific language governing permissions
-  and limitations under the License.
-  ***************************************************************************** */
 
   var __assign = function() {
       __assign = Object.assign || function __assign(t) {
@@ -676,7 +656,6 @@
 
   Object.defineProperty(exports, '__esModule', { value: true });
 
-}));
 //# sourceMappingURL=seamless.js.map
 
 export {polyfill$7};
